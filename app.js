a4629f9beae02dbd3bae29a8c1cef06ed8dfe825
Nydia Olmos <111654273+NydiaOlmos@users.noreply.github.com>
2025-02-26 22:49:12 -0600

    Update app.js
    Solución de problemas con el final del juego.

--- app.js
+++ app.js
@@ -33,19 +33,35 @@ function verificarIntento(){
 }
 
 function reiniciarJuego(){
-    condicionesInciales(); // Restablecemos las condiciones
     habilitaBoton('#reiniciar', true); // Deshabilitamos el botón de reiniciar
     habilitaBoton('#botonVerificar', false); // Habilitamos el botón de verificar
     limpiarCampo(); // Limpiamos el input
+    condicionesInciales(); // Restablecemos las condiciones
+    // console.log(listaNumerosSorteados)
     return;
 }
 
+function generarNumeroAleatorio(){
+    let numeroGenerado = Math.floor(Math.random() * maximo) + minimo; // Generamos un número aleatorio entre 1 y 10
+    if (listaNumerosSorteados.length == maximo){
+        habilitaBoton('#botonVerificar', true); // Deshabilitamos el botón de verificar
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles.');
+    }else {
+        if (listaNumerosSorteados.includes(numeroGenerado)){ // includes recorre la lista y checa si el numero existe
+            numeroGenerado = generarNumeroAleatorio(); // Recursividad
+        }else {
+            listaNumerosSorteados.push(numeroGenerado); // Agregamos el número a la lista de números sorteados
+        }
+    }
+    return numeroGenerado;
+}
+
 function condicionesInciales(){
+    asignarTextoElemento('h1', 'Juego del número secreto'); // Cambiamos el texto del título
+    asignarTextoElemento('p', `Escribe un número del ${minimo} al ${maximo}`); // Cambiamos el texto del párrafo
     numeroSecreto = generarNumeroAleatorio(); // Generamos un nuevo número secreto
     intentos = 1; // Reiniciamos el contador de intentos
     console.log(`Número secreto: ${numeroSecreto}`);
-    asignarTextoElemento('h1', 'Juego del número secreto'); // Cambiamos el texto del título
-    asignarTextoElemento('p', `Escribe un número del ${minimo} al ${maximo}`); // Cambiamos el texto del párrafo
     return;
 }
 
@@ -65,20 +81,6 @@ function asignarTextoElemento(elemento , texto){
     let elementoHtml = document.querySelector(elemento); // Seleccionamos el elemento
     elementoHtml.innerHTML = texto; // Cambiamos el contenido del elemento
     return;
-}
-
-function generarNumeroAleatorio(){
-    let numeroGenerado = Math.floor(Math.random() * maximo) + minimo; // Generamos un número aleatorio entre 1 y 10
-    if (listaNumerosSorteados.length == maximo){
-        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles.')
-    }else {
-        if (listaNumerosSorteados.includes(numeroGenerado)){ // includes recorre la lista y checa si el numero existe
-            numeroGenerado = generarNumeroAleatorio(); // Recursividad
-        }else {
-            listaNumerosSorteados.push(numeroGenerado); // Agregamos el número a la lista de números sorteados
-        }
-    }
-    return numeroGenerado;
 }
 
 
